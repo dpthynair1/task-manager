04005996d159a0a08b93539677904b5a83468164
Deepthy Nair <dpthynair@gmail.com>
2020-09-12 21:19:02 +0200

    Index moved to src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
-require('./src/db/mongoose')
-const User = require('./src/models/user')
-const Task = require('./src/models/task')
-const userRouter = require('./src/router/user')
-const taskRouter = require('./src/router/task')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+const userRouter = require('./router/user')
+const taskRouter = require('./router/task')
 
 const app = express()
 
